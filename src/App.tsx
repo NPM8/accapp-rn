188765c1782f2c92f9ed9a7f2c2efa50a3a202c9
npm8 <nnnp.dawid@gmail.com>
2018-12-19 09:43:50 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  public render() {
+    return (
+        <View style={{ marginTop: 50 }}>
+            <Text>Hello Expo!</Text>
+        </View>
+    );
+  }
+}
